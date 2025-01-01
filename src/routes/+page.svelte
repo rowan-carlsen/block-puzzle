d03284b952acdc1078b9917e2e56f6c3ef9035f9
Rowan Carlsen <rowan.carlsen@gmail.com>
2025-01-01 18:04:22 -0500

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,144 @@
 <script>
-	import { flip } from 'svelte/motion';
-	class Block {
-		constructor(width, height, x, y) {
-			this.width = width;
-			this.height = height;
-			this.x = x;
-			this.y = y;
+	import { crossfade } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
+	const [send, receive] = crossfade({
+		duration: (d) => Math.sqrt(d * 200),
+
+		fallback(node) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 600,
+				css: (t) => `
+				transform: ${transform} scale(${t});
+				opacity: ${t}
+			`
+			};
+		}
+	});
+	const blank = { coords: [3, 3], letter: '', blank: true };
+	let done = $state(false);
+	const grid = $state([
+		{
+			coords: [1, 3],
+			letter: 'o'
+		},
+		{
+			coords: [2, 2],
+			letter: 'p'
+		},
+		{
+			coords: [4, 4],
+			letter: 'e'
+		},
+		{
+			coords: [3, 1],
+			letter: 'n'
+		},
+		{
+			coords: [3, 2],
+			letter: 'd'
+		},
+		{
+			coords: [1, 1],
+			letter: 'o'
+		},
+		{
+			coords: [4, 2],
+			letter: 'o'
+		},
+		{
+			coords: [3, 4],
+			letter: 'r'
+		},
+		{
+			coords: [2, 1],
+			letter: ''
+		},
+		{
+			coords: [1, 2],
+			letter: ''
+		},
+		{
+			coords: [4, 1],
+			letter: ''
+		},
+		{
+			coords: [4, 3],
+			letter: ''
+		},
+		{
+			coords: [2, 3],
+			letter: ''
+		},
+		{
+			coords: [1, 4],
+			letter: ''
+		},
+		{
+			coords: [2, 4],
+			letter: ''
+		},
+		{
+			coords: [3, 3],
+			letter: '',
+			blank: true
+		}
+	]);
+
+	function canMove(block) {
+		return (
+			Math.abs(block.coords[0] - blank.coords[0]) + Math.abs(block.coords[1] - blank.coords[1]) ===
+			1
+		);
+	}
+	function move(block) {
+		if (!done && canMove(block)) {
+			[block.coords, blank.coords] = [blank.coords, block.coords];
+			checkIfDone();
 		}
 	}
-	let grid = $state({
-		a: new Block(1, 1, 1, 1),
-		b: new Block(1, 2, 2, 1),
-		c: new Block(2, 1, 3, 1),
-		d: new Block(1, 1, 5, 1),
-		e: new Block(1, 2, 1, 2),
-		f: new Block(2, 2, 3, 2),
-		g: new Block(1, 2, 5, 2),
-		h: new Block(1, 1, 1, 4),
-		i: new Block(2, 1, 3, 4),
-		j: new Block(1, 1, 5, 4),
-		k: new Block(2, 1, 1, 5),
-		l: new Block(2, 1, 3, 5),
-		m: new Block(1, 1, 5, 5)
-	});
-	let listGrid = $derived(Object.values(grid));
-	// let grid = $state([
-	// 'abccd',
-	// 'ebffg',
-	// 'e-ffg',
-	// 'h-iij',
-	// 'kkllm']);
+	function checkIfDone() {
+		const result = [];
+		for (let y = 1; y < 3; y++) {
+			for (let x = 1; x < 5; x++) {
+				const letterAt = grid.find((entry) => entry.coords[0] === x && entry.coords[1] === y);
+				result.push(letterAt.letter);
+			}
+		}
+		if (result.join('') === 'opendoor') {
+			done = true;
+		}
+	}
 </script>
 
 <main>
 	<h1>Door Puzzle</h1>
 	<div id="container">
-		{#each listGrid as block (block)}
-			<div
-				class="block"
-				style="--width:{block.width}; --height:{block.height}; --xPos: {block.x}; --yPos: {block.y}"
-			></div>
+		{#each grid as block, i (i)}
+			{#key block.coords}
+				<div
+					role="button"
+					tabindex="0"
+					onclick={() => move(block)}
+					onkeydown={(e) => {
+						console.log(e.key);
+					}}
+					class="block"
+					class:blank={block.blank}
+					style="--xPos: {block.coords[0]}; --yPos: {block.coords[1]}"
+					in:receive={{ key: i }}
+					out:send={{ key: i }}
+				>
+					{block.letter}
+				</div>
+			{/key}
 		{/each}
 	</div>
+	{#if done}
+		<h2 in:fade>The door opens...</h2>
+	{/if}
 </main>
 
 <style>
@@ -55,22 +152,64 @@
 		border-radius: 0.5em;
 	}
 	#container {
-		width: 25em;
-		height: 25em;
+		width: 20em;
+		height: 20em;
 		box-sizing: content-box;
 		border: 2px solid darkgrey;
 		margin: 5em auto 0 auto;
 		background: black;
 		display: grid;
-		grid-template: repeat(5, 5em) / repeat(5, 5em);
+		grid-template: repeat(4, 5em) / repeat(4, 5em);
+		position: relative;
+	}
+	#container::before,
+	#container::after {
+		position: absolute;
+		width: 5rem;
+		height: 5rem;
+		font-size: 5em;
+		color: gold;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		left: -5rem;
+		transform: translateY(-0.1em);
+	}
+	#container::before {
+		content: '\00BB';
+		top: 0;
+	}
+	#container::after {
+		content: '\00BB';
+		top: 5rem;
 	}
 	.block {
 		border: 2px solid transparent;
-		width: calc(var(--width) * 5em);
-		height: calc(var(--height) * 5em);
-		grid-column: var(--xPos) / span var(--width);
-		grid-row: var(--yPos) / span var(--height);
+		width: 5rem;
+		height: 5rem;
+		grid-column: var(--xPos);
+		grid-row: var(--yPos);
 		background-color: grey;
 		background-clip: content-box;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 4em;
+		position: relative;
+		cursor: pointer;
+		user-select: none;
+	}
+	.blank {
+		background: transparent;
+		z-index: -5;
+		cursor: default;
+	}
+	h2 {
+		width: max-content;
+		margin: 2em auto 0 auto;
+		font-size: 2em;
+		padding: 0.25em;
+		background: #fff9;
+		border-radius: 0.25em;
 	}
 </style>
